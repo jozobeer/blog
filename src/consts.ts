788d2656b99b7807efffc5d4befdf39df600c608
HONDA Zyun <36469485+hondazn@users.noreply.github.com>
2026-08-31 12:10:21 +0900

    Update SITE_DESCRIPTION for blog content

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "JOZO's blog";
-export const SITE_DESCRIPTION = 'チームJOZOのブログです。';
+export const SITE_DESCRIPTION = 'jozo.beer のブログです。';
